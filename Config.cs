e9cda88da01007fe6c116fb6a774635d5d97bf9d
Mustafa SAVAŞ <mustafavesavas1@gmail.com>
2025-04-30 11:35:06 +0300

    .

--- Config.cs
+++ Config.cs
@@ -2,6 +2,8 @@
 using Exiled.API.Interfaces;
 using System.ComponentModel;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace SpecialKeycardPermissions
 {
@@ -90,82 +92,8 @@ namespace SpecialKeycardPermissions
         ItemType.KeycardO5
         };
 
-        public KeycardPermissions[] KeycardPermissionsExp { get; set; } =
-        {
-        KeycardPermissions.None,                 // 0: No permissions
-        KeycardPermissions.Checkpoints,          // 1: Opens checkpoints
-        KeycardPermissions.ExitGates,            // 2: Opens Gate A and Gate B
-        KeycardPermissions.Intercom,             // 4: Opens the Intercom door
-        KeycardPermissions.AlphaWarhead,         // 8: Opens the Alpha Warhead detonation button on surface
-        KeycardPermissions.ContainmentLevelOne,  // 0x10: Opens Scp914Gate
-        KeycardPermissions.ContainmentLevelTwo,  // 0x20: Opens Containment Level Two areas
-        KeycardPermissions.ContainmentLevelThree,// 0x40: Opens Containment Level Three areas
-        KeycardPermissions.ArmoryLevelOne,       // 0x80: Opens Light Containment armory
-        KeycardPermissions.ArmoryLevelTwo,       // 0x100: Opens Heavy Containment armories
-        KeycardPermissions.ArmoryLevelThree,     // 0x200: Opens MicroHID room or 3X and Jailbird Locker
-        KeycardPermissions.ScpOverride           // 0x400: Checkpoints access for SCP
-        };
+        public KeycardPermissions[] KeycardPermissionsExp { get; set; } = [.. Enum.GetValues(typeof(KeycardPermissions)).Cast<KeycardPermissions>()];
 
-        public DoorType[] DoorTypeExp { get; set; } =
-        {
-        DoorType.UnknownDoor,
-        DoorType.Scp914Door,
-        DoorType.GR18Inner,
-        DoorType.Scp049Gate,
-        DoorType.Scp049Armory,
-        DoorType.Scp079First,
-        DoorType.Scp079Second,
-        DoorType.Scp096,
-        DoorType.Scp079Armory,
-        DoorType.Scp106Primary,
-        DoorType.Scp106Secondary,
-        DoorType.Scp173Gate,
-        DoorType.Scp173Connector,
-        DoorType.Scp173Armory,
-        DoorType.Scp173Bottom,
-        DoorType.GR18Gate,
-        DoorType.Scp914Gate,
-        DoorType.Scp939Cryo,
-        DoorType.CheckpointLczA,
-        DoorType.CheckpointLczB,
-        DoorType.EntranceDoor,
-        DoorType.EscapePrimary,
-        DoorType.EscapeSecondary,
-        DoorType.GateA,
-        DoorType.GateB,
-        DoorType.HczArmory,
-        DoorType.HeavyContainmentDoor,
-        DoorType.HeavyBulkDoor,
-        DoorType.HIDChamber,
-        DoorType.HIDUpper,
-        DoorType.HIDLower,
-        DoorType.Intercom,
-        DoorType.LczArmory,
-        DoorType.LczCafe,
-        DoorType.LczWc,
-        DoorType.LightContainmentDoor,
-        DoorType.NukeArmory,
-        DoorType.NukeSurface,
-        DoorType.PrisonDoor,
-        DoorType.SurfaceGate,
-        DoorType.Scp330,
-        DoorType.Scp330Chamber,
-        DoorType.SurfaceDoor,
-        DoorType.CheckpointEzHczA,
-        DoorType.CheckpointEzHczB,
-        DoorType.UnknownGate,
-        DoorType.UnknownElevator,
-        DoorType.ElevatorGateA,
-        DoorType.ElevatorGateB,
-        DoorType.ElevatorNuke,
-        DoorType.ElevatorScp049,
-        DoorType.ElevatorLczA,
-        DoorType.ElevatorLczB,
-        DoorType.CheckpointArmoryA,
-        DoorType.CheckpointArmoryB,
-        DoorType.Airlock,
-        DoorType.Scp173NewGate,
-        DoorType.EscapeFinal
-        };
+        public DoorType[] DoorTypeExp { get; set; } = [.. Enum.GetValues(typeof(DoorType)).Cast<DoorType>()];
     }
 }
